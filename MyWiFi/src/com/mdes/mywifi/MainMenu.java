364579aac9fc66fd853baebb4457c17515344c75
irenemds <irene.mds@gmail.com>
2014-03-18 17:04:29 +0100

    Añadido ListView. Muestra lista de redes disponibles con parámetros.

--- MyWiFi/src/com/mdes/mywifi/MainMenu.java
+++ MyWiFi/src/com/mdes/mywifi/MainMenu.java
@@ -1,21 +1,38 @@
 package com.mdes.mywifi;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.ListView;
 import android.widget.ToggleButton;
 
 public class MainMenu extends Activity {
 
 	private ToggleButton wifiOn;
 	private WifiManager  wifiManager;
+	List<ScanResult> resultWifiList;
+    ArrayList<String> wifiList;
+	private ListView lista;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_menu);
+		
+		 lista = (ListView) findViewById(R.id.List1);
 		
 		//ToggleButton
 		wifiOn = (ToggleButton)findViewById(R.id.wifiOn);
@@ -31,9 +48,51 @@ public class MainMenu extends Activity {
 	            }
 
 	        });
-	}
+	     //BroadcastReceiver para cambios externos
+	     registerReceiver(new BroadcastReceiver()
+	        {
+	            @Override
+	            public void onReceive(Context c, Intent intent) 
+	            {
+	               resultWifiList = wifiManager.getScanResults();
+	               wifiList = new ArrayList<String>();
+	               resultWifiList = wifiManager.getScanResults();
+	               Log.i("App irene", "\n        Number Of Wifi connections :"+resultWifiList.size()+"\n\n");
+	               for(int i = 0; i < resultWifiList.size(); i++){
+	               	wifiList.add(resultWifiList.get(i).toString());
+	               }
+	               final StableArrayAdapter adapter = new StableArrayAdapter(c,
+	                       android.R.layout.simple_list_item_1, wifiList);
 
-
-}
+	               lista.setAdapter(adapter);
+	            }
+	        }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)); 
+      wifiManager.startScan();
+      	    
+	    }
 	
+	private class StableArrayAdapter extends ArrayAdapter<String> {
 
+	    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+	    public StableArrayAdapter(Context context, int textViewResourceId,
+	        List<String> objects) {
+	      super(context, textViewResourceId, objects);
+	      for (int i = 0; i < objects.size(); ++i) {
+	        mIdMap.put(objects.get(i), i);
+	      }
+	    }
+
+	    @Override
+	    public long getItemId(int position) {
+	      String item = getItem(position);
+	      return mIdMap.get(item);
+	    }
+
+	    @Override
+	    public boolean hasStableIds() {
+	      return true;
+	    }
+
+	  }
+}
