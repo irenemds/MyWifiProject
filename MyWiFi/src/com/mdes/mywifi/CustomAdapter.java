f6885814deef8d6f5ea17710043370555ce1c3e1
irenemds <irene.mdes@gmail.com>
2014-10-26 21:44:47 +0100

    Imágenes en función de la potencia en WifiList

--- MyWiFi/src/com/mdes/mywifi/CustomAdapter.java
+++ MyWiFi/src/com/mdes/mywifi/CustomAdapter.java
@@ -62,17 +62,17 @@ public class CustomAdapter extends BaseAdapter {
 			holder.level.setText(Integer.toString(result.level)+" dBm");
 			holder.SSID.setText(result.SSID); 
 			//	Diferentes imágenes de prueba en función del nivel de señal
-//			if(result.level <=0 && result.level >-30){
-//				holder.barraNivel.setImageResource(R.drawable.signal5);}
-//			else if(result.level <=-30 && result.level >-50){
-//				holder.barraNivel.setImageResource(R.drawable.signal4);}
-//			else if(result.level <=-50 && result.level >-70){
-//				holder.barraNivel.setImageResource(R.drawable.signal3);}
-//			else if(result.level <=-70 && result.level >-80){
-//				holder.barraNivel.setImageResource(R.drawable.signal2);}
-//			if(result.level <=-80){
-//				holder.barraNivel.setImageResource(R.drawable.signal1);
-//			}
+			if(result.level <=0 && result.level >-30){
+				holder.barraNivel.setImageResource(R.drawable.signal5);}
+			else if(result.level <=-30 && result.level >-50){
+				holder.barraNivel.setImageResource(R.drawable.signal4);}
+			else if(result.level <=-50 && result.level >-70){
+				holder.barraNivel.setImageResource(R.drawable.signal3);}
+			else if(result.level <=-70 && result.level >-80){
+				holder.barraNivel.setImageResource(R.drawable.signal2);}
+			if(result.level <=-80){
+				holder.barraNivel.setImageResource(R.drawable.signal1);
+			}
 		
 		return vistaReciclada;
 		
