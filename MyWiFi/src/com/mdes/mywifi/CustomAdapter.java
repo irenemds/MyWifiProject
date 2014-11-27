4e36c11fee7e3007a3c33281535c7c4145ee4b8b
irenemds <irene.mdes@gmail.com>
2014-11-27 21:38:18 +0100

    Nueva clase para gestión e imágenes potencia wifi

--- MyWiFi/src/com/mdes/mywifi/CustomAdapter.java
+++ MyWiFi/src/com/mdes/mywifi/CustomAdapter.java
@@ -17,14 +17,6 @@ import android.widget.TextView;
 public class CustomAdapter extends BaseAdapter {
 	Context contexto;
 	List<ScanResult> scanResult;
-	int[] signals = new int[]{
-			R.drawable.signal1,
-			R.drawable.signal2,
-			R.drawable.signal3,
-			R.drawable.signal4,
-			R.drawable.signal5
-	};
-
 
 	public CustomAdapter (Context contexto, List<ScanResult> scanResult){
 		this.contexto = contexto;
@@ -66,18 +58,21 @@ public class CustomAdapter extends BaseAdapter {
 				holder.level.setTypeface(null, Typeface.BOLD);
 				holder.SSID.setTypeface(null, Typeface.BOLD);
 			}
-			//	Diferentes imágenes en función del nivel de señal
-			if(result.level <=0 && result.level >-30){
-				holder.barraNivel.setImageResource(R.drawable.signal5);}
-			else if(result.level <=-30 && result.level >-50){
-				holder.barraNivel.setImageResource(R.drawable.signal4);}
-			else if(result.level <=-50 && result.level >-70){
-				holder.barraNivel.setImageResource(R.drawable.signal3);}
-			else if(result.level <=-70 && result.level >-80){
-				holder.barraNivel.setImageResource(R.drawable.signal2);}
-			if(result.level <=-80){
-				holder.barraNivel.setImageResource(R.drawable.signal1);
-			}
+			
+			holder.barraNivel.setImageResource(WifiLevelImage.getWifiLevelImage(result.level));
+			
+//			//	Diferentes imágenes en función del nivel de señal
+//			if(result.level <=0 && result.level >-30){
+//				holder.barraNivel.setImageResource(R.drawable.signal5);}
+//			else if(result.level <=-30 && result.level >-50){
+//				holder.barraNivel.setImageResource(R.drawable.signal4);}
+//			else if(result.level <=-50 && result.level >-70){
+//				holder.barraNivel.setImageResource(R.drawable.signal3);}
+//			else if(result.level <=-70 && result.level >-80){
+//				holder.barraNivel.setImageResource(R.drawable.signal2);}
+//			if(result.level <=-80){
+//				holder.barraNivel.setImageResource(R.drawable.signal1);
+//			}
 
 			if(posicion % 2 == 0){
 				vistaReciclada.setBackgroundColor(color.darker_gray);
